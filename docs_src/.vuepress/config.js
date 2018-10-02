d03210643c8abeaaa9d0711b71d1c86ac34aacc9
Caleb Harris <caleb.harris@mac.com>
2018-10-02 15:58:12 -0400

    Add support for disclosable answer blocks

--- docs_src/.vuepress/config.js
+++ docs_src/.vuepress/config.js
@@ -1,7 +1,22 @@
+const mdContainer = require("markdown-it-container");
+
 module.exports = {
   base: "/fp_book_club_ts/",
   dest: "docs",
   markdown: {
+    config: md => {
+      // Creates hidden-by-default, expandable blocks, meant for exercise answers
+      md.use(mdContainer, "answer", {
+        marker: "?",
+        render (tokens, idx) {
+          if (tokens[idx].nesting === 1) {
+            return '<details><summary>Answer</summary>\n';
+          } else {
+            return '</details>\n';
+          }
+        }
+      });
+    },
     lineNumbers: true
   },
   themeConfig: {
