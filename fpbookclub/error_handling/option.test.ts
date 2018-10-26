f6d84c447ebc9d6a3e1d803495bdc7f867c1fa1b
Caleb Harris <caleb.harris@mac.com>
2018-10-26 11:20:30 -0400

    Refactor and comment Option

--- fpbookclub/error_handling/option.test.ts
+++ fpbookclub/error_handling/option.test.ts
@@ -1,76 +1,75 @@
 import {
   Option,
-  Some,
-  None,
-  NONE,
   lift,
+  none,
+  some
 } from "./option";
 
 describe("filter()", () => {
   test("returns None for a None", () => {
-    expect(NONE.filter(x => true)).toEqual(NONE);
+    expect(none().filter(x => true)).toEqual(none());
   });
 
   test("returns None if the predicate returns false", () => {
-    expect(new Some("").filter(s => s.length > 0)).toEqual(NONE);
+    expect(some("").filter(s => s.length > 0)).toEqual(none());
   });
 
   test("returns Some if the predicate returns true", () => {
-    expect(new Some("").filter(s => s.length === 0)).toEqual(new Some(""));
+    expect(some("").filter(s => s.length === 0)).toEqual(some(""));
   });
 });
 
 describe("flatMap()", () => {
-  const thingWithName = new Some({ name: "Thomas" });
-  const nameless = new Some({ species: "bird" });
+  const thingWithName = some({ name: "Thomas" });
+  const nameless = some({ species: "bird" });
 
   const extractName = (thing: any): Option<string> => {
     if (typeof thing === "object" && typeof thing.name === "string") {
-      return new Some(thing.name);
+      return some(thing.name);
     } else {
-      return NONE;
+      return none();
     }
   }
 
   test("applies the function on a Some", () => {
-    expect(thingWithName.flatMap(extractName)).toEqual(new Some("Thomas"));
-    expect(nameless.flatMap(extractName)).toEqual(NONE);
+    expect(thingWithName.flatMap(extractName)).toEqual(some("Thomas"));
+    expect(nameless.flatMap(extractName)).toEqual(none());
   });
 });
 
 describe("getOrElse()", () => {
   test("returns the value of a Some", () => {
-    expect(new Some("hello").getOrElse(() => "world")).toEqual("hello");
+    expect(some("hello").getOrElse(() => "world")).toEqual("hello");
   });
 
   test("returns the default for None", () => {
-    expect(NONE.getOrElse(() => "world")).toEqual("world");
+    expect(none<string>().getOrElse(() => "world")).toEqual("world");
   });
 });
 
 describe("map()", () => {
   test("applies the function for on a Some", () => {
-    expect(new Some("hello").map(s => s.length)).toEqual(new Some(5));
+    expect(some("hello").map(s => s.length)).toEqual(some(5));
   });
 
   test("just returns None for a None", () => {
-    expect(NONE.map((s: string) => s.length)).toEqual(new None());
+    expect(none<string>().map(s => s.length)).toEqual(none());
   });
 });
 
 describe("orElse()", () => {
   test("returns the default for a None", () => {
-    expect(NONE.orElse(() => new Some(true))).toEqual(new Some(true));
+    expect(none<boolean>().orElse(() => some(true))).toEqual(some(true));
   });
 
   test("returns the Some for a Some", () => {
-    expect(new Some(true).orElse(() => new Some(false))).toEqual(new Some(true));
+    expect(some(true).orElse(() => some(false))).toEqual(some(true));
   });
 });
 
 describe("lift()", () => {
   test("successfully transforms Math.abs", () => {
     const absOpt = lift(Math.abs);
-    expect(absOpt(new Some(-3))).toEqual(new Some(3));
+    expect(absOpt(some(-3))).toEqual(some(3));
   });
 });
--- fpbookclub/error_handling/option.ts
+++ fpbookclub/error_handling/option.ts
@@ -1,35 +1,64 @@
+/**
+ * A type representing either the presence (`Some`) or absence (`None`) of a
+ * value
+ **/
 export type Option<A> = Some<A> | None<A>;
 
+/**
+ * Base trait for `Option` data constructors. Most methods should have an
+ * explicit `this` parameter to ensure they can only be called on an `Option`
+ **/
 abstract class OptionBase<A> {
+
+  /**
+   * If `this` is a `Some` and the value passes the predicate, returns `this`.
+   * Otherwise returns `None`.
+   **/
   filter(this: Option<A>, p: (a: A) => boolean): Option<A> {
-    if (this.tag === "some") {
-      if (p(this.value)) return this;
-      else return NONE;
-    }
-    return NONE;
+    if (this.tag === "some" && p(this.value)) return this;
+    return none();
   }
 
+  /**
+   * If `this` is a `Some`, returns the result of applying `f` to the value.
+   * Otherwise returns `None`.
+   **/
   flatMap<B>(this: Option<A>, f: (a: A) => Option<B>): Option<B> {
-    if (this.tag === "some") return f(this.value);
-    return NONE;
+    if (this.tag === "none") return none();
+    return f(this.value);
   }
 
+  /**
+   * If `this` is a `Some`, returns the value. Otherwise, returns the result of
+   * `onEmpty`.
+   **/
   getOrElse<T extends U, U>(this: Option<T>, onEmpty: () => U): U {
-    if (this.tag === "some") return this.value;
-    return onEmpty();
+    if (this.tag === "none") return onEmpty();
+    return this.value;
   }
 
+  /**
+   * If `this` is a `Some`, returns a new `Some` containing the result of
+   * applying `f` to the value. Otherwise returns `None`.
+   **/
   map<B>(this: Option<A>, f: (a: A) => B): Option<B> {
-    if (this.tag === "none") return NONE;
-    return new Some(f(this.value));
+    if (this.tag === "none") return none();
+    return some(f(this.value));
   }
 
+  /**
+   * Returns `this` if it is a `Some`. Otherwise returns the result of calling
+   * `ou`.
+   **/
   orElse<T extends U, U>(this: Option<T>, ou: () => Option<U>): Option<U> {
     if (this.tag === "none") return ou();
     return this;
   }
 }
 
+/**
+ * Data constructor representing the presence of a value of type `A`
+ **/
 export class Some<A> extends OptionBase<A> {
   readonly tag: "some" = "some";
   readonly value: A;
@@ -40,12 +69,35 @@ export class Some<A> extends OptionBase<A> {
   }
 }
 
+/**
+ * Data constructor representing the absence of a value of type `A`
+ **/
 export class None<A> extends OptionBase<A> {
+
+  /**
+  * Value that is always returned in case of a `None`
+  **/
+  static readonly NONE: Option<never> = new None();
+
   readonly tag: "none" = "none";
+
+  private constructor() {
+    super();
+  }
 }
 
-export const NONE: Option<never> = new None();
+/**
+ * Smart constructor for None, which always returns `None.NONE`
+ **/
+export const none: <A>() => Option<A> = () => None.NONE;
 
-export function lift<A, B>(f: (a: A) => B): (o: Option<A>) => Option<B> {
-  return o => o.map(f);
-}
+/**
+ * Smart constructor for `Some`
+ **/
+export const some: <A>(a: A) => Option<A> = a => new Some(a);
+
+/**
+ * Lift `f` into the `Option` context
+ **/
+export const lift: <A, B>(f: (a: A) => B) => (o: Option<A>) => Option<B> =
+  f => o => o.map(f);
