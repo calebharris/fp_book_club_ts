e537ade7d315107f164aca8fec1410f22a551eb5
Caleb Harris <caleb.harris@mac.com>
2018-10-30 13:40:05 -0400

    Add skeleton Either implementation

---
+++ fpbookclub/error_handling/either.test.ts
@@ -0,0 +1,22 @@
+import * as ei from "./either";
+type Either<E, A> = ei.Either<E, A>;
+const { left, right, ...either } = ei;
+
+describe("Try()", () => {
+  const tryDecodeURI = (s: string) => either.Try(() => decodeURI(s));
+
+  test("returns Right on a successful execution", () => {
+    expect(tryDecodeURI("example")).toEqual(right("example"));
+  });
+
+  test("returns Left on a thrown exception", () => {
+    expect(tryDecodeURI("%")).toEqual(left(new URIError("URI malformed")));
+  });
+
+  test("works for code that abuses throw", () => {
+    expect(either.Try(() => {
+      throw "This is not an error";
+    })).toEqual(left(new Error("This is not an error")));
+  });
+});
+
