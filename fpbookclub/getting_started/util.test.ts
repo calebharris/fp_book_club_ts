93fa0c5430c3d426d8ef967f3efa155effcdfa61
Caleb Harris <caleb.harris@mac.com>
2018-11-05 22:20:07 -0500

    Code style and comments for util.ts

--- fpbookclub/getting_started/util.test.ts
+++ fpbookclub/getting_started/util.test.ts
@@ -2,34 +2,26 @@ import { compose, curry, partial1, uncurry } from "./util";
 
 describe("curry()", () => {
   test("returns two nested functions", () => {
-    function add(x: number, y: number): number {
-      return x + y
-    };
-    const curried_add = curry(add);
-    const result = curried_add(1)(1);
+    const add = (x: number, y: number) => x + y;
+    const curriedAdd = curry(add);
+    const result = curriedAdd(1)(1);
     expect(result).toEqual(add(1, 1));
   });
 });
 
 describe("uncurry()", () => {
   test("returns a flat function", () => {
-    const curried_add: (x: number) => (y: number) => number = (x) => {
-      return (y: number) => x + y;
-    };
-    const uncurried_add = uncurry(curried_add);
-    const result = uncurried_add(1, 1);
-    expect(result).toEqual(curried_add(1)(1));
+    const curriedAdd = (x: number) => (y: number) => x + y;
+    const uncurriedAdd = uncurry(curriedAdd);
+    const result = uncurriedAdd(1, 1);
+    expect(result).toEqual(curriedAdd(1)(1));
   });
 });
 
 describe("compose()", () => {
   test("returns a function equal to f and then g", () => {
-    function addTen(x: number): number {
-      return x + 10;
-    };
-    function triple(x: number): number {
-      return x * 3;
-    };
+    const addTen = (x: number) => x + 10;
+    const triple = (x: number) => x * 3;
     const addTenAndTriple = compose(triple, addTen);
     const result = addTenAndTriple(1);
     expect(result).toEqual(33);
--- fpbookclub/getting_started/util.ts
+++ fpbookclub/getting_started/util.ts
@@ -1,23 +1,45 @@
-export function curry<A, B, C>(f: (a: A, b: B) => C): (a: A) => (b: B) => C {
-  return function(a: A) {
-    return partial1(a, f);
-  };
-}
+/**
+ * util.ts - some useful functional programming utilities
+ */
 
-export function partial1<A, B, C>(a: A, f: (a: A, b: B) => C): (b: B) => C {
-  return function(b) {
-    return f(a, b);
-  };
-}
+/**
+ * Given a function of two arguments, return a function of one argument which
+ * returns another function of one argument which returns the same result as
+ * the provided function.
+ *
+ * For example, if `c = f(a, b)`, `g = curry(f)`, and `d = g(a)(b)`, then
+ * `c === d`.
+ */
+export const curry: <A, B, C>(f: (a: A, b: B) => C) => (a: A) => (b: B) => C =
+  f => a => partial1(a, f);
 
-export function uncurry<A, B, C>(f: (a: A) => (b: B) => C): (a: A, b: B) => C {
-  return function(a: A, b: B): C {
-    return f(a)(b);
-  };
-}
+/**
+ * Given a function of two arguments, and a value compatible with the leftmost
+ * argument, return a function of one argument that has the same result.
+ *
+ * For example, if `c = f(a, b)`, `g = partial1(a, f)`, and `d = g(b)`, then
+ * `c === d`.
+ */
+export const partial1: <A, B, C>(a: A, f: (a: A, b: B) => C) => (b: B) => C =
+  (a, f) => b => f(a, b);
 
-export function compose<A, B, C>(f: (b: B) => C, g: (a: A) => B): (a: A) => C {
-  return function(a: A) {
-    return f(g(a));
-  }
-}
+/**
+ * Given a function of the same shape as that returned by `curry`, return a
+ * function of two arguments that has the same result.
+ *
+ * For example, if `c = f(a)(b)`, `g = uncurry(f)`, and `d = g(a, b)`, then
+ * `c === d`.
+ */
+export const uncurry: <A, B, C>(f: (a: A) => (b: B) => C) => (a: A, b: B) => C =
+  f => (a, b) => f(a)(b);
+
+/**
+ * Given two functions, where the second's return type is compatible with the
+ * first's argument type, return a single function that combines both
+ * computations.
+ *
+ * For example, if `b = g(a)`, `c = f(b)`, `h = compose(f, g)`, and `d = h(a)`,
+ * then `c === d`.
+ */
+export const compose: <A, B, C>(f: (b: B) => C, g: (a: A) => B) => (a: A) => C =
+  (f, g) => a => f(g(a));
