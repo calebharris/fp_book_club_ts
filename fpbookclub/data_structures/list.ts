051cfc601a1929d4d86adea6051bb271a26b88b5
Caleb Harris <caleb.harris@mac.com>
2018-11-30 16:31:01 -0600

    Refactor and completely document list.ts

--- fpbookclub/data_structures/list.ts
+++ fpbookclub/data_structures/list.ts
@@ -38,51 +38,60 @@ export class Cons<A> {
 /**
  * Creates a List from a variable number of arguments.
  */
-export function List<A>(...vals: A[]): List<A> {
+export const List = <A>(...vals: A[]): List<A> => {
   if (vals.length === 0)
     return Nil;
   else
     return new Cons(vals[0], List(...vals.slice(1)));
-}
+};
 
-export function addCorresponding(a1: List<number>, a2: List<number>): List<number> {
+/**
+ * Given two lists of numbers, return a list containing the sums of
+ * corresponding elements. If one list is longer than the other, the longer
+ * list's excess elements will be ignored.
+ */
+export const addCorresponding = (a1: List<number>, a2: List<number>): List<number> => {
   return zipWith(a1, a2, (n1, n2) => n1 + n2);
-}
+};
 
-export function addOne(l: List<number>): List<number> {
+/**
+ * Given a list of numbers, return a list containing the result of adding one
+ * to each element.
+ */
+export const addOne = (l: List<number>): List<number> => {
   return map(l, a => a + 1);
-}
+};
 
 /**
  * Creates a new list by appending a2 to a1
  */
-export function append<A>(a1: List<A>, a2: List<A>): List<A> {
+export const append = <A>(a1: List<A>, a2: List<A>): List<A> => {
   return foldRight(a1, a2, (a, b) => new Cons(a, b));
-}
+};
 
 /**
  * Returns a flattened List containing all elements of the given sublists in order
  */
-export function concat<A>(ll: List<List<A>>): List<A> {
+export const concat = <A>(ll: List<List<A>>): List<A> => {
   return foldRight(ll, List(), (a, b) => append(a, b));
-}
+};
 
 /**
  * Returns a new list missing the first `n` elements of `l`
  */
-export function drop<A>(l: List<A>, n: number): List<A> {
+export const drop = <A>(l: List<A>, n: number): List<A> => {
   if (l.tag === "nil")
       throw new Error("Attempt to drop from empty list");
 
   if (n <= 0) return l;
   else return drop(l.tail, n - 1);
-}
+};
 
 /**
  * Returns a new list missing the first contiguous sequence of elements that
  * match the predicate `p`
  */
-export function dropWhile<A>(l: List<A>, p: (a: A) => boolean): List<A> {
+export const dropWhile = <A>(l: List<A>, p: (a: A) => boolean): List<A> => {
   if (l.tag === "nil")
       throw new Error("Attempt to drop from empty list");
 
@@ -93,9 +102,13 @@ export function dropWhile<A>(l: List<A>, p: (a: A) => boolean): List<A> {
     }
 
   return l;
-}
+};
 
-export function hasSubsequence<A>(sup: List<A>, sub: List<A>): boolean {
+/**
+ * Returns `true` if `sup` contains the entirety of `sub`, in order and without
+ * interruption, starting in any position. Returns `false` otherwise.
+ */
+export const hasSubsequence = <A>(sup: List<A>, sub: List<A>): boolean => {
   const folded = foldLeft(sup, sub, (rem, cur) => {
     if (rem.tag === "nil")
       return Nil;
@@ -105,12 +118,12 @@ export function hasSubsequence<A>(sup: List<A>, sub: List<A>): boolean {
       return sub;
   });
   return folded === Nil;
-}
+};
 
 /**
  * Returns all but the last element of `l`
  */
-export function init<A>(l: List<A>): List<A> {
+export const init = <A>(l: List<A>): List<A> => {
   if (l.tag === "nil")
     throw new Error("Attempt to get init of empty list");
 
@@ -118,105 +131,117 @@ export function init<A>(l: List<A>): List<A> {
     return Nil;
 
   return new Cons(l.head, init(l.tail));
-}
+};
 
-export function filter<A>(l: List<A>, p: (a: A) => boolean): List<A> {
+/**
+ * Returns a list containing only the elemtns of `l` for which the predicate `p`
+ * is `true`.
+ */
+export const filter = <A>(l: List<A>, p: (a: A) => boolean): List<A> => {
   return flatMap(l, a => p(a) ? List(a) : List());
-}
+};
 
-export function flatMap<A, B>(l: List<A>, f: (a: A) => List<B>): List<B> {
+/**
+ * Returns a list where each element `a` of the input list, `l`, has been replaced
+ * by the elements of the list resulting from applying `f` to `a`.
+ */
+export const flatMap = <A, B>(l: List<A>, f: (a: A) => List<B>): List<B> => {
   return foldRight(l, List(), (a, acc) => append(f(a), acc));
-}
+};
 
 /**
  * Folds every element of the list into a single value by recursively applying
  * the provided function, starting at the left-hand side of the list.
  */
-export function foldLeft<A, B>(l: List<A>, z: B, f: (b: B, a: A) => B): B {
+export const foldLeft = <A, B>(l: List<A>, z: B, f: (b: B, a: A) => B): B => {
   let state = z;
   while (l.tag !== "nil") {
     state = f(state, l.head);
     l = l.tail;
   }
   return state;
-}
+};
 
 /**
  * Folds every element of the list into a single value by recursively applying
  * the provided function, starting at the right-hand side of the list.
  */
-export function foldRight<A, B>(l: List<A>, z: B, f: (a: A, b: B) => B): B {
+export const foldRight = <A, B>(l: List<A>, z: B, f: (a: A, b: B) => B): B => {
   return foldLeft(reverse(l), z, (b, a) => f(a, b));
-}
+};
 
 /**
  * Returns the length of l
  */
-export function length<A>(l: List<A>): number {
+export const length = <A>(l: List<A>): number => {
   return foldLeft(l, 0, (len, a) => len + 1);
-}
+};
 
 /**
  * Returns a new list where each element is the result of applying `f`
  * to the corresponding element in `la`
  */
-export function map<A, B>(la: List<A>, f: (a: A) => B): List<B> {
+export const map = <A, B>(la: List<A>, f: (a: A) => B): List<B> => {
   return flatMap(la, a => List(f(a)));
-}
+};
 
 /**
  * Multiplies a list of numbers together
  */
-export function product(ns: List<number>): number {
+export const product = (ns: List<number>): number => {
   return foldLeft(ns, 1.0, (prod, n) => n * prod);
-}
+};
 
 /**
  * Returns the reverse of a list
  */
-export function reverse<A>(l: List<A>): List<A> {
+export const reverse = <A>(l: List<A>): List<A> => {
   return foldLeft(l, List(), (rev, a) => new Cons(a, rev));
-}
+};
 
 /**
  * Replaces the head of a list with a different value
  */
-export function setHead<A>(l: List<A>, a: A): List<A> {
+export const setHead = <A>(l: List<A>, a: A): List<A> => {
   if (l.tag === "nil")
     throw new Error("Attempt to set head of empty list");
 
   return new Cons(a, l.tail);
-}
+};
 
 /**
  * Adds up a list of numbers
  */
-export function sum(ns: List<number>): number {
+export const sum = (ns: List<number>): number => {
   return foldLeft(ns, 0, (tot, n) => n + tot);
-}
+};
 
 /**
  * Returns the tail of a list
  */
-export function getTail<A>(l: List<A>): List<A> {
+export const getTail = <A>(l: List<A>): List<A> => {
   if (l.tag === "nil")
     throw new Error("Attempt to take tail of empty list");
 
   return l.tail;
-}
+};
 
-export function toString(l: List<number>): List<string> {
+/**
+ * Retuns a list containing the string representations of the elements of the
+ * input list.
+ */
+export const toString = (l: List<number>): List<string> => {
   return map(l, a => a.toString());
-}
+};
 
 /**
  * Returns a list comprising the results of applying `f` to corresponding
  * elements of the provided lists
  */
-export function zipWith<A, B, C>(
+export const zipWith = <A, B, C>(
     la: List<A>,
     lb: List<B>,
-    f: (a: A, b: B) => C): List<C> {
+    f: (a: A, b: B) => C): List<C> => {
   return reverse(foldLeft<A, [List<C>, List<B>]>(
     la,
     [Nil, lb],
@@ -227,4 +252,4 @@ export function zipWith<A, B, C>(
         return [acc, rem];
     },
   )[0]);
-}
+};
